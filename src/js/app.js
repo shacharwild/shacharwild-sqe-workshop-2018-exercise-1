71527821dcd8fcd6834a61d30c4651456ce548fd
shacharwild <wildsha@post.bgu.ac.il>
2018-11-21 20:54:19 +0200

    This is assignment one for the course.

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+import $ from 'jquery';
+import {parseCode} from './code-analyzer';
+//import {startBuildingTable} from './code-analyzer';
+
+
+$(document).ready(function () {
+    $('#codeSubmissionButton').click(() => {
+        let codeToParse = $('#codePlaceholder').val();
+        // let parsedCode = parseCode(codeToParse);
+        // $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        //result=[];
+        let  result =  parseCode(codeToParse);
+        makeTable(result);
+
+    });
+});
+
+
+//make table
+function makeTable(result) {
+    var html = '<table>';
+    html += '<tr>';
+    for( var j in result[0] ) {
+        html += '<th>' + j + '</th>';
+    }
+    html += '</tr>';
+    for( var i = 0; i < result.length; i++) {
+        html += '<tr>';
+        for( j in result[i] ) {
+            let text=result[i][j];
+            html += '<td >' + text + '</td>';
+        }
+    }
+    html += '</table>';
+    document.getElementById('finalTable').innerHTML = html;
+}
